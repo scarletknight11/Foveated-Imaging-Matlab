274d2a03252efc0fe75901a28663acdd0eeb0a18
Sanjit Singh <ss3448@scarletmail.rutgers.edu>
2022-09-12 23:38:15 -0400

    Update getpixel.m

--- getpixel.m
+++ getpixel.m
@@ -9,20 +9,12 @@ i5 = imread("iron-man-marvel-chrome-theme-wallpaper5.png");
 m = cat(4,i1,i2,i3,i4,i5);
 
 %create rgb stacks, rgb stacks will be 3
-red = m(:,:,1);
-green = m(:,:,2);
-blue = m(:,:,3);
-
-%create array of zeros and pass size of m within filler
-filler = zeros(size(m),'uint8');
-
-%Concatenate each RGB with filler and m
-rgbImageR = cat(3,m,filler,filler);
-rgbImageG = cat(3,filler,m,filler);
-rgbImageB = cat(3,filler,filler,m);
+red = m(:,:,:,1);
+green = m(:,:,:,2);
+blue = m(:,:,:,3);
 
 %compute all colors into one
-RGB = red + green + blue;
+RGB = red+green+blue;
 imshow(RGB);
 
 %specify the rows, columns, rgb and image number with m using indexing
@@ -33,4 +25,4 @@ b = m(:,:,:,5);
 %image filter to simulate the blur at that given location
 a(5:200,8:300,:) = b(5:200,8:300,:);
 
-imshow(a)
+imshow(a);
