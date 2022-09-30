af912211de1c06145ddc0ea2664dfdd49de279f1
Sanjit Singh <ss3448@scarletmail.rutgers.edu>
2022-09-30 01:02:09 -0400

    Update getpixel.m

--- getpixel.m
+++ getpixel.m
@@ -8,14 +8,24 @@ i5 = imread("iron-man-marvel-chrome-theme-wallpaper5.png");
 %concatened array full of images
 m = cat(4,i1,i2,i3,i4,i5);
 
-%create rgb stacks, rgb stacks will be 3
-red = m(:,:,:,1);
-green = m(:,:,:,2);
-blue = m(:,:,:,3);
+%check each rgb invidually
+red = m(:,:,1);
+green = m(:,:,2);
+blue = m(:,:,3);
 
-%compute all colors into one
-RGB = red+green+blue;
-imshow(RGB);
+%create/update rgb stacks, rgb stacks will be 5
+red = m(:,:,1,:);
+green = m(:,:,2,:);
+blue = m(:,:,3,:);
+
+r = squeeze(red);
+g = squeeze(green);
+b = squeeze(blue);
+
+rgb = cat(3,r,g,b);
+%[r,g,b] =  imsplit(rgb);
+
+rgb = m(:,:,:,3);
 
 %specify the rows, columns, rgb and image number with m using indexing
 a = m(:,:,:,2);
